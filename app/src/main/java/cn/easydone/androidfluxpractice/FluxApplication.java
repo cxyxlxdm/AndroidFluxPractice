ebb2e839e4634312bbcb4c32c98c5cda0437e2ca
liangzhitao <woshiliangzhitao@gmail.com>
2016-04-05 17:43:24 +0800

    init nuwa in branch master.

---
+++ app/src/main/java/cn/easydone/androidfluxpractice/FluxApplication.java
@@ -0,0 +1,23 @@
+package cn.easydone.androidfluxpractice;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Environment;
+
+import cn.jiajixin.nuwa.Nuwa;
+
+/**
+ * Created by Android Studio
+ * User: liangzhitao
+ * Date: 16/4/5
+ * Time: 下午5:37
+ */
+public class FluxApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Nuwa.init(this);
+        Nuwa.loadPatch(this, Environment.getExternalStorageDirectory().getAbsolutePath().concat("/patch.jar"));
+    }
+}
